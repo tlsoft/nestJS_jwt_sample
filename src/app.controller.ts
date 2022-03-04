a32a3709729a67756c56f42d1a3a2646ea2f7c71
Georg Polak <georg.polak@tlsoft.at>
2022-03-04 14:43:38 +0100

    add sample code

---
+++ src/app.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { AuthService } from './auth/auth.service';
+
+@Controller()
+export class AppController {
+  constructor(
+    private authService: AuthService,
+    private readonly appService: AppService,
+  ) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  // @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
